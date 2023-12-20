7ae301bc9a82489e9b409ce757c7cb5ad0abd24a
brian houston morrow <bhm@brianmorrow.net>
2023-12-20 01:07:07 -0600

    2023 day 12 part 2

--- day_12.nim
+++ day_12.nim
@@ -6,6 +6,11 @@ import sequtils
 import sugar
 import algorithm
 import bitops
+import threadpool
+import deques
+import tables
+import system
+import strformat
 
 const TEST_INPUT = """
 ???.### 1,1,3
@@ -16,24 +21,11 @@ const TEST_INPUT = """
 ?###???????? 3,2,1
 """
 
-proc isValidLine(line: string, gaps: seq[int]): bool =
-    var 
-        counts = newSeq[int]()
-        curCount = 0
-    
-    for c in line:
-        if c == '#':
-            inc curCount
-        else:
-            if curCount > 0:
-                counts.add(curCount)
-                curCount = 0
-    if curCount > 0:
-        counts.add(curCount)
-    
-    return counts == gaps
-
 iterator combos(n: int): seq[char] =
+    # there are 2^n combinations
+    # and 2 options, so use
+    # binary rep of each number up to 2^n
+    # to generate the combos
     let totalCombos = 1 shl n
     for i in 0 ..< totalCombos:
         var combo = newSeq[char]()
@@ -44,8 +36,8 @@ iterator combos(n: int): seq[char] =
                 combo.add('.')
         yield combo
 
-proc replaceFromChoice(line: string, choice: seq[char]): string = 
-    var 
+proc replaceFromChoice(line: string, choice: seq[char]): string =
+    var
         r = line.toSeq()
         i = 0
 
@@ -57,74 +49,89 @@ proc replaceFromChoice(line: string, choice: seq[char]): string =
             r[li] = l
     return r.join
 
-proc isValidLineWithChoice(line: string, choice: seq[char], gaps: seq[int]): bool =
-    var 
-        counts = newSeq[int]()
-        curCount = 0
-        choiceIdx = 0
+var cache = newTable[string, TableRef[seq[int], int]]()
+proc countValidSolutions(line: string, spans: seq[int]): int =
+    if line in cache:
+        if spans in cache[line]:
+            return cache[line][spans]
+
+    if line.len == 0:
+        if spans.len == 0:
+            return 1
+        return 0
+
+    if spans.len == 0:
+        if line.contains("#"):
+            return 0
+        return 1
+
+    let
+        c = line[0]
+        rest = line[1 .. ^1]
     
-    for i, c in line.pairs:
-        if counts.len > gaps.len:
-            return false
-        if c == '?':
-            if choice[choiceIdx] == '#':
-                inc curCount
-            else:
-                if curCount > 0:
-                    counts.add(curCount)
-                    curCount = 0
-            inc choiceIdx
-            continue
-
-        if c == '#':
-            inc curCount
-        else:
-            if curCount > 0:
-                counts.add(curCount)
-                curCount = 0
-
-    if curCount > 0:
-        counts.add(curCount)
+    if c == '.':
+        cache[line] = newTable([(spans, countValidSolutions(rest, spans))])
+        return cache[line][spans]
     
-    return counts == gaps
+    if c == '#':
+        let curSpan = spans[0]
+        if line.len >= curSpan and line[0 .. curSpan-1].allIt(it != '.') and (line.len == curSpan or line[curSpan] != '#'):
+            if line.len == curSpan:
+                cache[line] = newTable([(spans, countValidSolutions("", spans[1 .. ^1]))])
+                return cache[line][spans]
+            cache[line] = newTable([(spans, countValidSolutions(line[curSpan+1.. ^1], spans[1 .. ^1]))])
+            return cache[line][spans]
+        return 0
 
-proc checkPermutations(line: string, gaps: seq[int]): int =
-    var result = 0
-    let unknown = line.filterIt(it == '?')
-    for choice in combos(unknown.len):
-        if isValidLineWithChoice(line, choice, gaps):
-            result += 1
-    return result
+    if c == '?':
+        cache[line] = newTable([(spans, countValidSolutions(&"#{rest}", spans) + countValidSolutions(&".{rest}", spans))])
+        return cache[line][spans]
+    return 0
 
 proc partOne() =
     let fn = "./input/day_12.txt"
     #let fn = TEST_INPUT
 
-    var 
+    var
         map = newSeq[(string, seq[int])]()
         sum = 0
 
     withStream(f, fn, fmRead):
         for line in lines(f):
             if line != "":
-                let 
+                let
                     parts = line.split(" ")
                     st = parts[0]
-                    gaps = parts[1].split(",").mapIt(it.parseInt())
-                map.add((st, gaps))
+                    spans = parts[1].split(",").mapIt(parseInt(it))
+                sum += countValidSolutions(st, spans)
 
-    for m in map:
-        sum += checkPermutations(m[0], m[1])
     echo "Part one: ", sum
+
+proc unfold(info: (string, seq[int])): (string, seq[int]) =
+    let result1 = repeat(info[0] & "?", 5)[0 .. ^2]
+    let result2 = repeat(info[1], 5).foldl(a.concat(b))
+    return (result1, result2)
 
 proc partTwo() =
     let fn = "./input/day_12.txt"
-    
+    #let fn = TEST_INPUT
+
+    var
+        map = newSeq[(string, seq[int])]()
+        sum = 0
+
     withStream(f, fn, fmRead):
         for line in lines(f):
-            echo line
-    echo "Part two: "
+            if line != "":
+                let
+                    parts = line.split(" ")
+                    st = parts[0]
+                    spans = parts[1].split(",").mapIt(parseInt(it))
+                    unfolded = unfold((st, spans))
+                sum += countValidSolutions(unfolded[0], unfolded[1])
+
+    echo "Part two: ", sum
 
 when isMainModule:
-    partOne()
-    #partTwo()
+    #partOne()
+    partTwo()
